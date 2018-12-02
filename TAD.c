7618c823ead8b845b502c989b642fd582b85d32b
Yatex <40961682+Yatex@users.noreply.github.com>
2018-12-02 18:12:24 -0300

    Update TAD.c

--- TAD.c
+++ TAD.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "TAD.h"
+
+typedef struct flightsToAirport * flightsToAirportADT;
+
+typedef struct airport * aiportADT;
+
+typedef struct listCDT * listADT;
+
+typedef struct listCDT
+{
+	size_t week[7][2]; //una matriz de 7 lugares para la semana y los 2 para el cabotaje e iternacional
+	size_t composition[6];
+	aiportADT firstAirport;
+}listCDT;
+
+typedef struct flightsToAirport
+{
+	char * oaci;
+	size_t takeoffs;
+	size_t landings;
+	flightsToAirportADT nextFlights;
+}flightsToAirport;
+
+typedef struct airport
+{
+	char * oaci;
+	size_t from;
+	size_t to;
+	char * name;
+	airportADT nextAirport;
+	flightsToAirportADT firstFlight;
+}airport;
 
