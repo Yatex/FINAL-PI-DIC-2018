bb66beec0e1cd40c0e970b44e2f11be85e1d8605
nachgrasso <40865466+nachgrasso@users.noreply.github.com>
2018-12-07 16:49:13 -0300

    Update OpenFiles.c

--- OpenFiles.c
+++ OpenFiles.c
@@ -7,44 +7,43 @@
 #define MAX_LENGTH 150
 
 airportADT fillAirports(char* filename,int* error){
-	FILE *fp = fopen(filename,"rt");
+	FILE *fp = fopen(filename,"r");
 	airportADT airports = newAirport();
 	if (!airports) {
-		*error=2;  //ERROR 2 ERROR CREATING LIST
+		(*error)=2;  //ERROR 2 ERROR CREATING LIST
 		return NULL;
 	}
 	char allData[MAX_LENGTH];
 	fgets(allData,MAX_LENGTH,fp);
 	while(fgets(allData,MAX_LENGTH,fp)){
 		if (insertAirport(airports,allData,error) && error != 0) {
-			*error=3; //ERROR 3 ERROR COPYING
+			(*error)=3; //ERROR 3 ERROR COPYING
 		}
 	}
 	if (*error>0) {
-		*error=3;  //ERROR 3 ERROR COPYING
+		(*error)=3;  //ERROR 3 ERROR COPYING
 	}
 	fclose(fp);
 	return airports;
 }
 
 flightsADT fillFlights(char* filename, int* error){
-	FILE *fp = fopen(filename,"rt");
+	FILE *fp = fopen(filename,"r");
 	flightsADT flights = newFlights();
 	if (!flights) {
-		*error=2; //ERROR 2 ERROR CREATING LIST
+		(*error)=2; //ERROR 2 ERROR CREATING LIST
 		return NULL;
 	}
 	char allData[MAX_LENGTH];
 	fgets(allData,MAX_LENGTH,fp);
 	while(fgets(allData,MAX_LENGTH,fp)){
 		if (insertFlight(flights,allData,error) && error != 0){
-			*error=3;  //ERROR 3 ERROR COPYING
+			(*error)=3;  //ERROR 3 ERROR COPYING
 		}
 	}
 	if (*error!=0) {
-		*error=3;//ERROR 3 ERROR COPYING
+		(*error)=3;//ERROR 3 ERROR COPYING
 	}
 	fclose(fp);
 	return flights;
 }
-
