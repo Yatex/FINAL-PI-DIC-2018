deb6eeb412dcf4fef3c976c1afa9cde0bc150e6f
Yatex <40961682+Yatex@users.noreply.github.com>
2018-12-06 23:54:53 -0300

    Update AirportADT.h

--- AirportADT.h
+++ AirportADT.h
@@ -1,30 +1,43 @@
-//
-//  AirportADT.h
-//  TPFINALPI2018
-//
-//  Created by Igancio Grasso on 04/12/2018.
-//
-
-#ifndef AirportADT_h
-#define AirportADT_h
-#define FALSE 0
-#define TRUE !FALSE
+#ifndef Airport_h
+#define Airport_h
 #define Max_Oaci 5
 #define Max_Deno 70
-#define Max_Prov 19 
+#define Max_Prov 19
 
-#include <stdio.h>
-
-
+typedef struct airportNode* airNode;
 
 typedef struct airportCDT* airportADT;
 
-airportADT newAirport(void 	);
+typedef struct airportFormat{
+	char oaci[Max_Oaci];
+	char denomination[Max_Deno];
+	char province[Max_Prov];
+}airportFormat;
+
+typedef struct airportNode{
+	airportFormat data;
+	airNode next;
+}airportNode;
+
+typedef struct airportCDT{
+	airNode first;
+	airNode iterator;
+}airportCDT;
+
+airportADT newAirport(void);
+
+airportFormat newAirportFormat(void);
+
 int insertAirport(airportADT airports,char* data,int* errorAdding);
+
 void freeAirports(airportADT airports);
-airportFormat StoreAsAirData(char* text);
+
+airportFormat intoAirportFormat(char* text);
+
 void toBeginAirport(airportADT airports);
+
 int hasNextAirport(airportADT airports);
+
 airportFormat nextAirport(airportADT airports);
 
 #endif /* AirportADT_h */
