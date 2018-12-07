98eb6bd614c71f2693a12b32dd0bde171ded1acd
Yatex <40961682+Yatex@users.noreply.github.com>
2018-12-06 23:53:08 -0300

    Update AirportADT.c

--- AirportADT.c
+++ AirportADT.c
@@ -1,116 +1,89 @@
-//
-//  AirportADT.c
-//  TPFINALPI2018
-//
-//  Created by Igancio Grasso on 04/12/2018.
-//
+#include "Airport.h"
+#include <stdio.h>
 
-#include "AirportADT.h"
-
+#define MAX_CAMPOS 3
+#define MAX_SIZE 150
+#define FALSE 0
+#define TRUE !FALSE
 #define OACI 1
 #define DENOMINATION 4
 #define PROVINCE 21
 
-typedef struct airportNode{
-    airportFormat data;
-    struct airportNode* next;
-}airportNode;
-
-typedef airportNode* airNode;
-
-typedef struct airportCDT{
-    struct airportNode* first;
-    struct airportNode* iterator;
-}airportCDT;
-
-typedef struct airpData{
-    char oaci[Max_Oaci];
-    char denomination[Max_Deno];
-    char province[Max_Prov];
-}airportFormat;
-
-
-
 airportADT newAirport(){
-    return calloc(1,sizeof(airportCDT));
+	return calloc(1,sizeof(airportCDT));
 }
 
+airportFormat newAirportFormat(void){
+	return calloc(1, sizeof(airportFormat));
+}
 
 static airNode insertAirportRec(airNode first, airportFormat elem, int* errorAdding){
-    int result;
-    if (first == NULL || (result == strcmp(first.data.oaci,elem.oaci)) > 0) {
-        airNode newNode = malloc(sizeof(struct airportNode));
-        if (newNode == NULL) {
-            (*errorAdding)=TRUE;
-            return NULL;
-        }
-        newNode->data = elem;
-        newNode->next= first;
-        *errorAdding = FALSE;
-        return newNode;
-    }
-    
-    first->next = insertAirportRec(first->next,elem,errorAdding);
-    return first;
+	int result;
+	if (first == NULL || (result == strcmp(first.data.oaci,elem.oaci)) > 0) {
+		airNode newNode = malloc(sizeof(airportNode));
+		if (newNode == NULL) {
+			(*errorAdding)=1;
+			return NULL;
+		}
+		newNode->data = elem;
+		newNode->next= first;
+		*errorAdding = 0;
+		return newNode;
+	}
+	first->next = insertAirportRec(first->next,elem,errorAdding);
+	return first;
 }
 
 airportFormat intoAirportFormat(char * data){
-    char dataMatriz[MAX_CAMPOS][MAX_SIZE];
-    int i=0;
-    int j=0;
-    char* token = strtok(data,";");
-    while(token != NULL){
-        if (i==OACI || i==DENOMINATION || i==PROVINCE) {
-            strcpy(dataMatriz[j],token);
-            j++
-        }
-        token = strtok(NULL,";");
-        i++;
-    }
-    airportFormat newAirport;
-    strcpy(newAirport.oaci,dataMatriz[OACI]);
-    strcpy(newAirport.denomination,dataMatriz[DENOMINATION]);
-    strcpy(newAirport.province,dataMatriz[PROVINCE]);
-    
-    return newAirport;
-    
+	char dataMatriz[MAX_CAMPOS][MAX_SIZE];
+	int i=0;
+	int j=0;
+	char* token = strtok(data,";");
+	while(token != NULL){
+		if (i==OACI || i==DENOMINATION || i==PROVINCE) {
+			strcpy(dataMatriz[j],token);
+			j++;
+		}
+		token = strtok(NULL,";");
+		i++;
+	}
+	airportFormat newAirport = newAirportFormat(void);
+	strcpy(newAirport.oaci,dataMatriz[0]);
+	strcpy(newAirport.denomination,dataMatriz[1]);
+	strcpy(newAirport.province,dataMatriz[2]);
+	return newAirport;   
 }
 
 int insertAirport(airportADT airports, char * data, int * errorAdding){
-    airportFormat elem = intoAirportFormat(data);
-
-    if (*(elem.oaci)!= 0) {
-        airports->first = insertAirportRec(airports->first,elem, errorAdding);
-        return TRUE;
-    }
-    return FALSE;
+	airportFormat elem = intoAirportFormat(data);
+	if (*(elem.oaci)!= 0) {
+		airports->first = insertAirportRec(airports->first,elem, errorAdding);
+		return 1;
+	}
+	return 0;
 }
 
-
 void freeAirports(airportADT airports){
-    airNode current = airports->first;
-    airnode aux;
-    
-    while(current != NULL){
-        aux = current->next;
-        free(current);
-        current = aux;
-    }
-    free(airports);
-    
+	airNode current = airports->first;
+	airnode aux;
+	while(current != NULL){
+		aux = current->next;
+		free(current);
+		current = aux;
+	}
+	free(airports);
 }
 
 void toBeginAirport(airportADT airports){
-    airports->iterator = airports->first;
+	airports->iterator = airports->first;
 }
 
 int hasNextAirport(airportADT airports){
-    return airports->iterator != NULL;
+	return airports->iterator != NULL;
 }
 
 airportFormat nextAirport(airportADT airports){
-    airportFormat aux = airports->iterator->data;
-    airports->iterator = airports->iterator->next;
-    return aux;
+	airportFormat aux = airports->iterator->data;
+	airports->iterator = airports->iterator->next;
+	return aux;
 }
-
