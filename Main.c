24f5f2fb70360e6b6f6687b1166013c495b91295
Yatex <40961682+Yatex@users.noreply.github.com>
2018-12-06 23:51:21 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,69 +2,118 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-#include "TAD.h"
-#include "TAD2.h"
-#include "getnum.h"
+#include "Airport.h"
+#include "Flights.h"
+#include "OpenFiles.h"
 
 int
-dateToDay(char* str)     //This tell us what day this is.
+dateToDay(dateType date)     //return 0 if its MONDAY, 1 TUESDAY, ... , 6 SUNDAY.
 {
-	int d=0, m=0, y=0, count=0;
-	d+=((str[0]-'0')*10+(str[1]-'0')-1);
-	m+=((str[3]-'0')*10+(str[4]-'0'));
-	y+=(2010+(str[9]-'0'));
+	int d=date.day, m=date.month, y=date.year, count=0;
 	int month[]={31,28,31,30,31,30,31,31,30,31,30,31};
-	for (size_t i = 1; i < m; i++)
+	for (size_t i = 0; i < m-1; i++)
 		count+=month[i];
 	if(y>2016 || (y==2016 && m>2))
 		count+=1;
 	count+=((y-2014)*365+d);
-	return (count+2)%7;         //The first day of 2014 is Wednesday, so we +2, so 0 es monday, 1 tuesday...
+	return (count+1)%7;
+}
+
+void
+freeAll(airportADT aeros, flightsADT vuelos){
+	freeAirports(aeros);
+	freeFlights(vuelos);
+	return;
+}
+
+int
+QuerryUNO(airNode a, fNode f){
+	FILE * querry;
+	querry=fopen("movimientos_aeropuerto.csv","wt");
+	if (querry == NULL)
+		return 1;
+	while(a != NULL){
+		int mov = 0;
+		while(f != NULL){
+			if(strcmp(a->data.oaci , f->data.origenOaci)==0 || strcmp(a->data.oaci , f->data.destinoOaci))
+				mov += 1;
+			f = f->next;
+		}
+		fputs(a->data.oaci , querry);
+		fputs(";" , querry);
+		fputs(a->data.denomination , querry);
+		fputs(";" , querry);
+		fprintf(querry , "%d\n" , mov);
+		a = a -> next;
+	}
+	fclose(querry);
+	return 0;
+}
+
+int
+QuerryDOS(flightsADT f){
+	FILE * querry;
+	char* vec[7] = {"lunes","martes","miercoles","jueves","viernes","sabado","domingo"};
+	querry=fopen("dia_semana.csv","wt");
+	if (querry == NULL)
+		return 1;
+	for(int i=0; i<7; i++){
+		fprintf(querry, "%s;%d;%d;%d \n", vec[i], f->week[i][0], f->week[i][1], (f->week[i][0] + f->week[i][1]));
+	}
+	fclose(querry);
+	return 0;
+}
+
+int
+QuerryTRES(flightsADT f){
+	FILE * querry;
+	char* vec[6] = {"Cabotaje;Regular;","Cabotaje;No Regular;","Cabotaje;Vuelo Privado;","Internacional;Regular;","Internacional;No Regular;","Internacional;Vuelo Privado;"};
+	querry=fopen("dia_semana.csv","wt");
+	if (querry == NULL)
+		return 1;
+	for(int i=0; i<7; i++){
+		fprintf(querry, "%s ; %d \n", vec[i], f->composition[i]);
+	}
+	fclose(querry);
+	return 0;
 }
 
 int
 main(void)
 {
-	int Error = 0;  //Depending in the number, its a different error. For example Error=1 will mean out of memory
-	int year = getint("Insert a year between 2014-2018 inclusive: \n"); //AVERIGUAR SI HACE FALTA HACER ESTO, lo de preguntar el año xq no lo vi en el enunciado	
-	ListADT2 list2=NULL;
-	ListADT list=createListAir(year, &Error);
-	if(Error == 0)
-		addMov(list, list2, list3, &Error);
-	if(Error != 0)
+	int error = 0;  //Depending in the number, its a different error. For example Error=1 will mean out of memory
+	airportADT aeros = fillAirports("aeropuertos.csv", &error);
+	flightsADT vuelos = NULL;
+	if(error != 0){
+		vuelos = fillFlights("movimientosR.csv", &error);
+	}
+	if(error != 0)
 	{
-		switch(Error)
+		switch(error)
 		{
 			case 1:
 				printf("No enough memory to continue");
 			case 2:
-				printf("");                             //    ESTO        
-			case 3:                                         //    SERIA
-				printf("");                             //    FRONT 
-			case 4:                                         //      -
-				printf("");                             //     END
-			case 5:
-				printf("");
+				printf("Error creating list");
+			case 3:
+				printf("Error copying the information");
 		}
 		return 1;
 	}
-	QuerryUNO(list);
-	QuerryDOS(list);
-	freeList(list);
-	QuerryTRES(list2);
-	freeList(list2);
+	if((aeros->first != NULL) && (vuelos ->first != NULL)){
+		if (QuerryUNO(aeros->first, vuelos->first))
+			printf("The file movimientos_aeropuerto.csv has been created succesfully\n");
+		else
+			printf("The file movimientos_aeropuerto.csv could not be created due to a lack of memory\n");
+		if (QuerryDOS(vuelos))
+			printf("The file dia_semana.csv has been created succesfully\n");
+		else
+			printf("The file dia_semana.csv could not be created due to a lack of memory\n");
+		if (QuerryTRES(vuelos))
+			printf("The file composicion.csv has been created succesfully\n");
+		else
+			printf("The file composicion.csv could not be created due to a lack of memory\n");
+		freeAll(aeros, vuelos);
+	}
 	return 0;
-}
-
-void
-createListAir(year, *Error)
-{
-	//ESTE ES UNO DE LOS QUE LEEN LOS ARCHIVOS
-}
-
-void
-addMov(list, list2, list3, *Error)
-{
-	//ESTE TAMBIEN LEE EL ARCHIVO MOV 
-	//ESTE LE MANDA LOS DATOS EN UN FOR A LA LISTA, LISTA2
 }
