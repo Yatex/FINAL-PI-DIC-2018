5a3b9dbb097636e8649fbc6dfb2928a9f8c4ca10
Yatex <40961682+Yatex@users.noreply.github.com>
2018-12-06 23:58:24 -0300

    Update flightsCDT.h

--- flightsCDT.h
+++ flightsCDT.h
@@ -1,25 +1,60 @@
-//
-//  flights.h
-//  TPFINALPI2018
-//
-//  Created by Igancio Grasso on 04/12/2018.
-//
+#ifndef Flights_h
+#define Flights_h
 
-#ifndef flightsCDT_h
-#define flightsCDT_h
-#define FALSE 0
-#define TRUE !FALSE
+typedef struct date{
+	unsigned day;
+	unsigned month;
+	unsigned year;
+}dateType;
 
-#include <stdio.h>
+typedef struct flight{
+	dateType fecha;
+	char *clase;
+	char *clasificacion;
+	char *tipo;
+	char *origenOaci;
+	char *destinoOaci;
+	char *nombre;
+}flightFormat;
 
+typedef struct flightNode* fNode;
 
+typedef struct flightNode{
+	flightFormat data;
+	fNode next;
+}flightNode;
+
+typedef struct flightsCDT* flightsADT;
+
+typedef struct flightsCDT{
+	int week[7][2];
+	int composition[6];
+	fNode first;
+	fNode iterator;
+}flightsCDT;
 
 flightsADT newFlights(void);
+
+fNode newFlight(void);
+
+void checkOaci(char oaci[]);
+
+int dateToDay(dateType date);
+
 void insertFlight(flightsADT flights, flightFormat movement, int *errorAdding);
+
 void freeFlights(flightsADT flights);
+
 void toBeginFlights(flightsADT flights);
+
 int hasNextFlight(flightsADT flights);
+
 flightFormat nextFlight(flightsADT flights);
 
+char* addIfSpace( char* data, int* spaceError);
+
+fNode intoFlightFormat( char* data, int* errorAdding);
+
+int getTypeComp( flightFormat elem);
 
 #endif /* flightsCDT_h */
